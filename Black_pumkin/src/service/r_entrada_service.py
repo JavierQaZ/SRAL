c6dcd0e6043c47cf9b91f21806b55f2136d65cb1
PipeOsaavedra <olivares.saavedra.f.24@gmail.com>
2024-06-02 23:48:29 -0400

    coloque unos trigger para que funcione lo que queremos

--- Black_pumkin/src/service/r_entrada_service.py
+++ Black_pumkin/src/service/r_entrada_service.py
@@ -65,3 +65,55 @@ def delete_r_entrada_service(HoraIngreso, RUT):
 #	"HoraIngreso": "2024-06-01T08:00:00",
 #	"RUT": "1111111-0"
 #}
+
+
+#CREATE TABLE registro_diario (
+#    ID INT AUTO_INCREMENT PRIMARY KEY,
+#    RUT VARCHAR(15),
+#    Fecha DATE,
+#    HoraEntrada DATETIME,
+#    HoraSalida DATETIME,
+#    FOREIGN KEY (RUT) REFERENCES empleados(RUT)
+#);
+
+#DELIMITER //
+#
+#CREATE TRIGGER after_insert_registroentrada
+#AFTER INSERT ON registroentrada
+#FOR EACH ROW
+#BEGIN
+#    DECLARE fecha_entrada DATE;
+#    SET fecha_entrada = DATE(NEW.HoraIngreso);
+#    
+#    UPDATE registro_diario
+#    SET HoraEntrada = NEW.HoraIngreso
+#    WHERE RUT = NEW.RUT
+#    AND Fecha = fecha_entrada;
+#    
+#    IF ROW_COUNT() = 0 THEN
+#        INSERT INTO registro_diario (RUT, Fecha, HoraEntrada)
+#        VALUES (NEW.RUT, fecha_entrada, NEW.HoraIngreso);
+#    END IF;
+#END//
+#
+#DELIMITER ;
+
+#
+#CREATE TRIGGER after_insert_registrosalida
+#AFTER INSERT ON registrosalida
+#FOR EACH ROW
+#BEGIN
+#    DECLARE fecha_salida DATE;
+#    SET fecha_salida = DATE(NEW.HoraSalida);
+#    
+#    UPDATE registro_diario
+#    SET HoraSalida = NEW.HoraSalida
+#    WHERE RUT = NEW.RUT
+#    AND Fecha = fecha_salida;
+#    
+#    IF ROW_COUNT() = 0 THEN
+#        INSERT INTO registro_diario (RUT, Fecha, HoraSalida)
+#        VALUES (NEW.RUT, fecha_salida, NEW.HoraSalida);
+#    END IF;
+#END//
+
