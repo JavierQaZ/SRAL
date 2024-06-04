2dfa26af3296a1eafd5bb9083098eb2eedeccffd
ChrisIgn <christian.delgado@alu.ucm.cl>
2024-06-03 22:36:53 -0400

    Update config.py

--- Black_pumkin/config.py
+++ Black_pumkin/config.py
@@ -5,7 +5,7 @@ class Config:
     # Configuración para MySQL usando Flask-MySQLdb
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root'#Intentar mantener esta clave puede variar segun la maquina
+    MYSQL_PASSWORD = '1607'#Intentar mantener esta clave puede variar segun la maquina
     MYSQL_DB = 'black_pumpkin'
 
 class DevelopmentConfig(Config):
