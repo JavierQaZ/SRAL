8ca1a3a2c09b8818bd36a9b63135b3df9e7804c9
ChrisIgn <christian.delgado@alu.ucm.cl>
2024-11-04 23:52:47 -0300

    a

--- Black_pumkin/app.py
+++ Black_pumkin/app.py
@@ -1,11 +1,14 @@
-#Encargado de lanzar toda la aplicacion
+from flask_jwt_extended import JWTManager
 from config import config
 from src import create_app
-
+from datetime import timedelta
 
 configuration = config['development']
 app = create_app(configuration)
 
+# Configurar JWT
+app.config['JWT_SECRET_KEY'] = 'tu_secreto_aqui'  # Cambia esto por una clave secreta segura
+jwt = JWTManager(app)
+
 if __name__ == '__main__':
-   
-    app.run(port = 5000, debug = True)
+    app.run(port=5000, debug=True)
